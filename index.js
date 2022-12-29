3b4ba4999f2a7554b2ef6fbcfb48498dbcac0fe3
nahumrud <nahumrud@hotmail.com>
2022-12-29 18:55:18 -0300

    1

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function miClosure () {
-    var x = 10;
-    return x;
+// function miClosure () {
+//     var x = 10;
+//     return x;
+// }
+
+// var x = miClosure();
+// console.log(x);
+
+class TicketManager{
+    enventos;
+    #precioBaseDeGanacia = 0.2;
+    constructor(eventos){
+        enventos ? this.enventos = eventos : this.enventos = []
+    }
+    get eventos(){
+        return this.#eventos;
+    }
 }
 
-var x = miClosure();
-console.log(x);
+const ticketManager = new TicketManager();
